80d8357f700157fcb9e72a5ec4e50b7bf9d260d9
DESKTOP-AF7B9H9\Admin <tangnh@newwave.vn>
2024-06-07 17:21:11 +0700

    add gitignore

--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754bdfb07a2b5a4152da8133caa9a012eb5fa533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a52fc7a25b51e72a7d0596a7b528e4bae9e8a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
