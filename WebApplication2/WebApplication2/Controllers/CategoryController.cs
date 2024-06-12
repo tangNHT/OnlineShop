45f5a4e1b83b0481049597e4af01586aae509984
DESKTOP-AF7B9H9\Admin <tangnh@newwave.vn>
2024-06-12 17:16:42 +0700

    Chia nhỏ layout, sử dụng DropdownList, gọi Ajax, xây dựng giao diện cho client

---
+++ WebApplication2/WebApplication2/Controllers/CategoryController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+	public class CategoryController : BaseController
+	{
+		// GET: CategoryController
+		public ActionResult Index(string searchString, int pageNumber = 1, int pageSize = 10)
+		{
+			//Tạo Model
+			var iplCategory = new CategoryModel();
+
+			var totalPages = iplCategory.TotalPages(pageSize);
+			var totalItems = iplCategory.TotalItems();
+			var categories = string.IsNullOrEmpty(searchString)
+							? iplCategory.ListAll(pageNumber, pageSize)
+							: iplCategory.ListAll(searchString, pageNumber, pageSize);
+
+			var model = new PaginationModel<Category>
+			{
+				Items = categories,
+				PageNumber = pageNumber,
+				TotalPages = totalPages,
+				TotalItems = totalItems
+			};
+			return View(model);
+		}
+
+		// GET: CategoryController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: CategoryController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: CategoryController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create(Category collection)
+		{
+			try
+			{
+				//Xử lý dữ liệu hợp lệ
+				if (ModelState.IsValid)
+				{
+					var model = new CategoryModel();
+					int res = await model.Create(collection.Name, collection.Alias, collection.ParentId, collection.Order, collection.Status);
+					if (res > 0)
+					{
+						TempData["SuccessMessage"] = "Thêm mới danh mục thành công";
+						//Chuyển đến Action khác
+						TempData["RedirectUrl"] = Url.Action("Index", "Category");
+					}
+					else
+					{
+						TempData["ErrorMessage"] = "Danh mục đã tồn tại";
+						ModelState.AddModelError("Create Category False", "Thêm mới không thành công");
+					}
+				}
+				//Trả lại trang với dữ liệu đã nhập và thông báo lỗi
+				return View(collection);
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: CategoryController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			var user = new CategoryModel().ViewDetail(id);
+			return View(user);
+		}
+
+		// POST: CategoryController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(Category collection)
+		{
+			try
+			{
+				//Xử lý dữ liệu hợp lệ
+				if (ModelState.IsValid)
+				{
+					var model = new CategoryModel();
+					var res = model.Update(collection);
+					if (res)
+					{
+						TempData["SuccessMessage"] = "Cập nhật danh mục thành công";
+						//Chuyển đến Action khác
+						TempData["RedirectUrl"] = Url.Action("Index", "Category");
+					}
+					else
+					{
+						TempData["ErrorMessage"] = "Cập nhật không thành công";
+						ModelState.AddModelError("Edit Category False", "Cập nhật không thành công");
+					}
+				}
+				//Trả lại trang với dữ liệu đã nhập và thông báo lỗi
+				return View(collection);
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// POST: CategoryController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id)
+		{
+			try
+			{
+				var model = new CategoryModel();
+				var result = model.Delete(id);
+				if (result)
+				{
+					TempData["SuccessMessage"] = "Xoá danh mục thành công";
+					TempData["RedirectUrl"] = Url.Action("Index", "Category");
+				}
+				else
+				{
+					TempData["ErrorMessage"] = "Xoá danh mục thành công";
+					ModelState.AddModelError("Delete Category False", "Xoá không thành công");
+				}
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		[HttpPost]
+		public JsonResult ChangeStatus (int id)
+		{
+			//Tạo model
+			var collection = new CategoryModel();
+			var result = collection.ChangeStatus(id);
+			//Dữ liệu gửi đi
+			return Json(new
+			{
+				status = result,
+			});
+		}
+	}
+}
---
+++ WebApplication2/WebApplication2/wwwroot/Admin/js/controller/categoryController.js
@@ -0,0 +1,36 @@
+﻿var category = {
+    //Khởi tạo và thiết lập sự kiện
+    init: function () {
+        category.registerEvent();
+    },
+    registerEvent: function () {
+        //Chọn các phần tử của class btn-active
+        //Huỷ bỏ tất cả các sự kiện click trước đó
+        //Đăng ký sự kiện click mới
+        $('.btn-active').off('click').on('click', function (e) {
+            //Ngăn chặn hành động mặc đinh của thẻ a
+            e.preventDefault();
+            var btn = $(this);
+            //Lấy giá trị của data-id
+            var id = btn.data('id');
+            $.ajax({
+                url: "Category/ChangeStatus", //Url thay đổi trạng thái
+                data: { id: id }, //Dữ liệu gửi đến
+                dataType: "json",//Định dạng dữ liệu là JSON
+                type: "POST",//Loại HTTP method để gửi yêu cầu
+                //Hàm callback được gọi khi Ajax thành công
+                success: function (response) {
+                    if (response.status == true) {
+                        btn.text('Active');
+                    } else {
+                        btn.text('Blocked');
+                    }
+                }
+            });
+        });
+    }
+}
+//Đảm bảo mã JavaScript chỉ được thực thi sau khi toàn bộ tài liệu HTML đã được tải
+$(document).ready(function () {
+    category.init();
+});
